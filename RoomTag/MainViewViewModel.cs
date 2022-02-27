cecde596884de9746b4c3fa74c94b9a46863171c
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2022-02-27 23:12:35 +0300

    Добавьте файлы проекта.

---
+++ RoomTag/MainViewViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomTag
+{
+    internal class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        //public Pipe Pipe { get; }
+        public List<FamilySymbol> Tags { get; }
+        public DelegateCommand SaveCommand { get; }
+        public FamilySymbol SelectedTagType { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            Pipe = SelectionUtils.GetObject<Pipe>(commandData, "Выберите трубу");
+            Tags = TagsUtils.GetRoomTagTypes(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Room> rooms = TagsUtils.GetRooms(_commandData);
+            var pipeCurve = pipeLocCurve.Curve;
+            var pipeMidPoint = (pipeCurve.GetEndPoint(0) + pipeCurve.GetEndPoint(1)) / 2;
+
+            using (var ts = new Transaction(doc, "Create tag"))
+            {
+                ts.Start();
+                IndependentTag.Create(doc, SelectedTagType.Id, doc.ActiveView.Id, new Reference(Pipe), false, TagOrientation.Horizontal, pipeMidPoint);
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ RoomTag/TagsUtils.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomTag
+{
+    internal class TagsUtils
+    {
+        public static List<FamilySymbol> GetRoomTagTypes(ExternalCommandData commandData)
+        {
+            
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            var roomTagTypes = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_RoomTags)
+                .OfClass(typeof(FamilySymbol))
+                .Cast<FamilySymbol>()
+                .ToList();
+            return roomTagTypes;
+
+        }
+
+
+        public static List<Room> GetRooms(ExternalCommandData commandData)
+        {
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            var rooms = new FilteredElementCollector(doc)
+                .OfClass(typeof(Room))
+                .OfType<Room>()
+                
+                .ToList();
+            return rooms;
+
+        }
+    }
+}
